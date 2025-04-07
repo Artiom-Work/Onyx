ce4b0e6f42a4c8f3155e44f5b4899251b9baa339
artiom mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-04-07 15:02:41 +0300

    added select for choosing a chat in the user field block

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const blockUserSelectChat = document.querySelector('.user-field__button-select-wrapper');
+console.log(blockUserSelectChat);
 //for block chats
 
 const swiper = new Swiper('.swiper', {
@@ -13,4 +14,10 @@ const swiper = new Swiper('.swiper', {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
+});
+
+// for select chat button in block user-field 
+
+blockUserSelectChat.addEventListener('click', function (e) {
+	blockUserSelectChat.querySelector('.decor-triangle').classList.toggle('decor-triangle--rotate');
 });
