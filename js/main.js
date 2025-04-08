20a86c3400c57f128edb95dd57611bd3ea1c2a8a
artiom mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-04-08 13:07:17 +0300

    added adaptive version 1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 'use strict';
 const blockUserSelectChat = document.querySelector('.user-field__button-select-wrapper');
-console.log(blockUserSelectChat);
+const userSelectChatButton = document.querySelector('.user-field__select');
+let clickSelectCounter = 0;
+
+
+
 //for block chats
 
 const swiper = new Swiper('.swiper', {
-	direction: 'horizontal',
-	slidesPerView: "auto",
-	centeredSlides: true,
+	direction: 'vertical',
+	slidesPerView: 1,
+	spaceBetween: 38,
+	centeredSlides: false,
 	loop: true,
 	grabCursor: true,
 
@@ -14,10 +19,37 @@ const swiper = new Swiper('.swiper', {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
+
+	breakpoints: {
+		0: {
+			direction: 'vertical'
+		},
+		1024: {
+			direction: 'horizontal',
+			slidesPerView: 2
+		}
+	}
 });
 
 // for select chat button in block user-field 
 
+userSelectChatButton.addEventListener('focus', function (e) {
+	if (e) {
+		blockUserSelectChat.querySelector('.decor-triangle').classList.add('decor-triangle--rotate');
+	}
+});
+userSelectChatButton.addEventListener('blur', function (e) {
+	if (e) {
+		blockUserSelectChat.querySelector('.decor-triangle').classList.remove('decor-triangle--rotate');
+		clickSelectCounter = 0;
+	}
+});
 blockUserSelectChat.addEventListener('click', function (e) {
-	blockUserSelectChat.querySelector('.decor-triangle').classList.toggle('decor-triangle--rotate');
+	clickSelectCounter = clickSelectCounter + 1;
+	if (clickSelectCounter > 1) {
+		blockUserSelectChat.querySelector('.decor-triangle').classList.remove('decor-triangle--rotate');
+		clickSelectCounter = 0;
+	} else {
+		blockUserSelectChat.querySelector('.decor-triangle').classList.add('decor-triangle--rotate');
+	}
 });
